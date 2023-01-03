097cb7a18165c91b3fd90a5e13787172163454e9
GH0ST <adamjamessherrill@gmail.com>
2023-01-03 14:57:07 -0600

    created footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 import './App.css';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+export default function Footer () {
+
+    return (
+        <footer className="page-footer pt-4 fixed-bottom">
+            <div className="footer-copyright text-center py-3">
+                © 2023 Copyright Adam Sherrill
+            </div>
+        </footer>
+    )
+}
